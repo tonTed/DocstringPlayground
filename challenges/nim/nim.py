cb926aae963b85e1bf62658e4cc32f78b36f0913
tonted <tblanco@student.42quebec.com>
2023-10-06 12:07:48 -0400

    add Game class with structure of engine

--- challenges/nim/nim.py
+++ challenges/nim/nim.py
@@ -1,11 +1,32 @@
 
 STICK_CHARACTER = '#'
 STICK_HEIGHT = 3
+STICK_START = 20
+INPUT_COMPUTER = ""
 
 def	stickDisplay(amount :int):
-    [print(f"{(STICK_CHARACTER + ' ') * amount}") for _ in range(STICK_HEIGHT)]
-    
+	[print(f"{(STICK_CHARACTER + ' ') * amount}") for _ in range(STICK_HEIGHT)]
+	
+class Game:
+	def __init__(self):
+		self.sticks = STICK_START
+
+	def turnComputer(self):
+		pass
+
+	def turnPlayer(self):
+		pass
+	
+	def start(self):
+		turnPlayer :bool = False
+		while (self.sticks > 0):
+			if (turnPlayer):
+				self.turnPlayer()
+			else:
+				self.turnComputer()
+			turnPlayer = not turnPlayer
+	
 
 if __name__ == '__main__':
 	stickDisplay(5)
-    
+	
