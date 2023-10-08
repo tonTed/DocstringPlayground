91f2132a78dd2b91074c455b9f682933d2eb4b04
tonted <tblanco@student.42quebec.com>
2023-10-08 07:31:32 -0400

    create architecture of class Game

--- challenges/nim/nim.py
+++ challenges/nim/nim.py
@@ -1,21 +1,33 @@
+import random
 
 STICK_CHARACTER = '#'
 STICK_HEIGHT = 3
 STICK_START = 20
 INPUT_COMPUTER = ""
 
+# TODO create class display
 def	stickDisplay(amount :int):
 	[print(f"{(STICK_CHARACTER + ' ') * amount}") for _ in range(STICK_HEIGHT)]
-	
+
 class Game:
 	def __init__(self):
 		self.sticks = STICK_START
 
 	def turnComputer(self):
-		pass
+		randomSticks = random.randint(1, 3)
+		#TODO manage amounts au sticks can't be less of 0
+		print(f"computer takes {randomSticks} sticks.")
+		self.sticks -= randomSticks
 
+	#TODO implement
 	def turnPlayer(self):
-		pass
+		self.turnComputer()
+
+	def end(self, turnPlayer :bool):
+		if turnPlayer:
+			print("Player win's!")
+		else:
+			print("Computer win's!")
 	
 	def start(self):
 		turnPlayer :bool = False
@@ -25,8 +37,10 @@ class Game:
 			else:
 				self.turnComputer()
 			turnPlayer = not turnPlayer
+		self.end(turnPlayer)
 	
 
 if __name__ == '__main__':
-	stickDisplay(5)
+	game :Game = Game()
+	game.start()
 	
