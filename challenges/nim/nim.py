7b0c31065317fff9f8e12acbd91f82624b269e4e
tonted <tblanco@student.42quebec.com>
2023-10-08 09:22:54 -0400

    add GameDisplay class, and ternary operator instead if/else

--- challenges/nim/nim.py
+++ challenges/nim/nim.py
@@ -6,10 +6,24 @@ STICK_START = 20
 INPUT_COMPUTER = ""
 
 # TODO create class display
-def	stickDisplay(amount: int):
-	[print(f"{(STICK_CHARACTER + ' ') * amount}") for _ in range(STICK_HEIGHT)]
 
-class Game:
+class GameDisplay:
+	@staticmethod
+	def	stickDisplay(amount: int):
+		[print(f"{(STICK_CHARACTER + ' ') * amount}") for _ in range(STICK_HEIGHT)]
+
+	@staticmethod
+	def __winDisplay(playerName: str):
+		print(f"{playerName} win's!")
+	
+	def computerWinDisplay(self):
+		self.__winDisplay("Computer")
+
+	def playerWinDisplay(self):
+		self.__winDisplay("Player")
+
+
+class Game(GameDisplay):
 	def __init__(self):
 		self.sticks = STICK_START
 
@@ -24,14 +38,12 @@ class Game:
 		self.turnComputer()
 
 	def end(self, turnPlayer: bool):
-		if turnPlayer:
-			print("Player win's!")
-		else:
-			print("Computer win's!")
+		turnPlayer if self.playerWinDisplay() else self.computerWinDisplay()
 	
 	def start(self):
 		turnPlayer: bool = False
 		while (self.sticks > 0):
+			self.stickDisplay(self.sticks)
 			if (turnPlayer):
 				self.turnPlayer()
 			else:
